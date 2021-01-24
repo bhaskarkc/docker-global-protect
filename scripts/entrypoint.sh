b916959b547c813efb30880d3f80b037bc765a86
Bhaskar K <xlinkerz@gmail.com>
2021-01-24 17:35:14 +1100

    type fix.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,7 @@
 _sha256() {
     echo 'Q' \
         | openssl s_client \
-        -connect globalprotect.domain.com.au:443 \
+        -connect "${SERVER}":443 \
         2> /dev/null \
         | openssl x509 -pubkey -noout \
         | openssl pkey -pubin -outform der \
